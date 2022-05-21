3c3865bf47e4555e9e61660962ba8e051b666a84
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-05-21 15:20:22 +0530

    Constructors in Java

--- OOPS_Concepts/src/Intro.java
+++ OOPS_Concepts/src/Intro.java
@@ -1,10 +1,8 @@
 public class Intro {
-
+     
     public static void main(String args[]){
-        Human obj = new Human();
-        obj.age = 21;
-        obj.weight = 78;
-        obj.sleep();
+        Human obj = new Human(10, 78);
+        System.out.println("age is " + obj.age);
     }
 
 }
@@ -13,6 +11,25 @@ class Human{
     // properties
     int age;
     int weight;
+
+    // default constructor
+
+    // No-arg Constructor
+
+    public Human() {
+        System.out.println("Inside the constructor");
+    }
+
+    // Paramterized Constructor
+    public Human(int age, int weight){
+        this.age = age;
+        this.weight = weight;
+    }
+
+    // Constructor Overloading
+    public Human(int age){
+        this.age = age;
+    }
 
     // behaviour
     void sleep(){
